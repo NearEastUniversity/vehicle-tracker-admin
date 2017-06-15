ee0b7ff65e660360f16786bf04380c21503035f8
drac <seyhan.drc@gmail.com>
2017-06-15 12:52:45 +0300

    init(project): create project skeleton

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Master from './components/Master'
+
+const App = () => (
+  <div>
+    <Master />
+  </div>
+)
+
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
---
+++ app/components/Master/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './style';
+
+const Master = () => (
+    <div className={style.app}>
+        <h2>Hello drac</h2>
+    </div>
+);
+
+export default Master;
