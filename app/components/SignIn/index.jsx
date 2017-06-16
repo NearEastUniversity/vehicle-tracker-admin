fffd34c423032a8fa9ea833f8fba53994b4bafc2
Vladislav Siumbeli <vladislavsiumbeli@gmail.com>
2017-06-16 14:18:53 +0300

    fix(SignIn) : fix z-index bug
    - fix align container
    - fix z-index bug

--- app/components/SignIn/index.jsx
+++ app/components/SignIn/index.jsx
@@ -2,35 +2,28 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
-import {GridList, GridTile} from 'material-ui/GridList';
 
 import style from './style';
 import logo from './img/logo.svg'
 
 const materialSignInStyles = {
-	gridList: {
-		top: 0,
-    margin: 'auto',
-    paddingTop: 150,
-  },
-	materialPaper: {
+
+	signIN: {
 		padding: 30,
 		width: 350,
 		height: 400,
-		float: 'right',
+		float: 'left',
+		zIndex: 5,
+		position: 'relative',
 	},
-	materialBlankPaper: {
-		textAlign: 'center',
+	blankDiv: {
 		paddingTop: 100,
-		height: 50,
 		marginTop: 15,
-		marginLeft: -20,
     width: 450,
     height: 370,
     backgroundColor: '#FFEF6A',
-	},
-	header: {
-		textAlign: 'center',
+    float: 'left',
+    zIndex: 1,
 	},
 	textFieldFirst: {
 		marginTop: 40,
@@ -54,40 +47,38 @@ const materialSignInStyles = {
 };
 
 const SignIn = () => (
-    <div className={style.app}>
-    	<GridList style={materialSignInStyles.gridList} >
+  <div className={style.app}>
+   	<div className={style.container}>
+			<Paper zDepth={4} style={materialSignInStyles.signIN} >
+				<h2>Sign In</h2>
+				<TextField 
+		    	hintText="email@example.com" 
+				  floatingLabelText="Email"
+				  floatingLabelStyle={materialSignInStyles.floatingLabelTextStyle} 
+				  style={materialSignInStyles.textFieldFirst} />
+				 <br />
+				<TextField 
+	    		hintText="********" 
+					floatingLabelText="Password"
+	    		type="password"
+			  	floatingLabelStyle={materialSignInStyles.floatingLabelTextStyle}
+					style={materialSignInStyles.textFieldSecond} />
+				<br />
+				<RaisedButton 
+			  	label="SIGN IN" 
+				  backgroundColor="#039BE5" 
+		    	labelColor="#ffffff" 
+				  fullWidth={true} 
+			  	style={materialSignInStyles.button} 
+				  labelStyle={materialSignInStyles.buttonText} />
+			</Paper>
 
-				  <Paper zDepth={4} style={materialSignInStyles.materialPaper} >
-				  	<h2 style={materialSignInStyles.header} >Sign In</h2>
-				    <TextField 
-				    	hintText="email@example.com" 
-				    	floatingLabelText="Email"
-				    	floatingLabelStyle={materialSignInStyles.floatingLabelTextStyle} 
-				    	style={materialSignInStyles.textFieldFirst} />
-				    <br />
-				    <TextField 
-				    	hintText="********" 
-				    	floatingLabelText="Password"
-				    	type="password"
-				    	floatingLabelStyle={materialSignInStyles.floatingLabelTextStyle}
-				    	style={materialSignInStyles.textFieldSecond} />
-				    <br />
-				    <RaisedButton 
-				    	label="SIGN IN" 
-				    	backgroundColor="#039BE5" 
-				    	labelColor="#ffffff" 
-				    	fullWidth={true} 
-				    	style={materialSignInStyles.button} 
-				    	labelStyle={materialSignInStyles.buttonText} />
-				  </Paper>
-
-				  <Paper zDepth={1} style={materialSignInStyles.materialBlankPaper}>
-				  	<img src={logo} alt={"logo"}/>
-				  	<h3 style={materialSignInStyles.adminText}>NEU Bus Tracker Admin</h3>
-				  </Paper>
-
-		  </GridList>
-    </div>
+			<Paper style={materialSignInStyles.blankDiv}>
+				 	<img src={logo} alt={"logo"}/>
+				  <h3 style={materialSignInStyles.adminText}>NEU Bus Tracker Admin</h3>
+			</Paper>
+		</div>
+	</div>
 );
 
 export default SignIn;
