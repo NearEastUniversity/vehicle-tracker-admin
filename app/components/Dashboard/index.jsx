d5e88c015e5cbe59c5da7260983513defb9720bb
Vladislav Siumbeli <vladislavsiumbeli@gmail.com>
2017-06-22 12:19:49 +0300

    feat(dashboard) : add dialog component
    - add dialog component to edit and unassign buttons

--- app/components/Dashboard/index.jsx
+++ app/components/Dashboard/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn, } from 'material-ui/Table';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import GridList from 'material-ui/GridList';
 import Paper from 'material-ui/Paper';
 import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 
 import ImageEdit from 'material-ui/svg-icons/image/edit';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
@@ -35,11 +36,27 @@ const materialuiDashboardStyle = {
   },
 }
 
+const vehicles = [
+  <MenuItem key={1} value={1} primaryText="Vehicles"  />,
+  <MenuItem key={2} value={2} primaryText="Vehicles1" />,
+  <MenuItem key={3} value={3} primaryText="Vehicles2" />,
+  <MenuItem key={4} value={4} primaryText="Vehicles3" />,
+  <MenuItem key={5} value={5} primaryText="Vehicles4" />,
+];
+
+const agents = [
+  <MenuItem key={1} value={1} primaryText="Agents"  />,
+  <MenuItem key={2} value={2} primaryText="Agents1" />,
+  <MenuItem key={3} value={3} primaryText="Agents2" />,
+  <MenuItem key={4} value={4} primaryText="Agents3" />,
+  <MenuItem key={5} value={5} primaryText="Agents4" />,
+];
+
 export default class Dashoboard extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {vehicleValue: 1, agentValue: 1};
+    this.state = {vehicleValue: 1, agentValue: 1, dialogEdit: false, dialogAlert: false};
   }
 
   vehicleChange(event, index, vehicleValue) {
@@ -50,7 +67,49 @@ export default class Dashoboard extends React.Component {
     this.setState({agentValue});
   }
 
+  dialogOpen() {
+    this.setState({dialogEdit: true});
+	}
+
+  dialogAlert() {
+    this.setState({dialogAlert: true});
+	}
+
+	dialogClose() {
+		this.setState({dialogEdit: false, dialogAlert: false});
+	}
+
   render() {
+    const editActions = [
+      <FlatButton
+        label="Cancel"
+        style={{color: "#747374"}}
+        primary={true}
+        onTouchTap={this.dialogClose.bind(this)}
+      />,
+      <FlatButton
+        label="Edit"
+        style={{color: "#039BE5"}}
+        primary={true}
+        onTouchTap={this.dialogClose.bind(this)}
+      />,
+    ];
+
+    const alertActions = [
+      <FlatButton
+        label="Cancel"
+        style={{color: "#747374"}}
+        primary={true}
+        onTouchTap={this.dialogClose.bind(this)}
+      />,
+      <FlatButton
+        label="Unassign"
+        style={{color: "#ff0000"}}
+        primary={true}
+        onTouchTap={this.dialogClose.bind(this)}
+      />,
+    ];
+
     return (
       <div className={style.app}>
 
@@ -65,11 +124,7 @@ export default class Dashoboard extends React.Component {
                 maxHeight={300}
                 value={this.state.agentValue}
                 onChange={this.agentChange.bind(this)}>
-                <MenuItem value={1} primaryText="Agents" />
-                <MenuItem value={2} primaryText="Agents1" />
-                <MenuItem value={3} primaryText="Agents2" />
-                <MenuItem value={4} primaryText="Agents3" />
-                <MenuItem value={5} primaryText="Agents4" />
+                {agents}
               </SelectField>
             </div>
 
@@ -81,11 +136,7 @@ export default class Dashoboard extends React.Component {
                 maxHeight={300}
                 value={this.state.vehicleValue}
                 onChange={this.vehicleChange.bind(this)}>
-                <MenuItem value={1} primaryText="Vehicles" />
-                <MenuItem value={2} primaryText="Vehicles1" />
-                <MenuItem value={3} primaryText="Vehicles2" />
-                <MenuItem value={4} primaryText="Vehicles3" />
-                <MenuItem value={5} primaryText="Vehicles4" />
+                {vehicles}
               </SelectField>
             </div>
             <div className={style.createButton}>
@@ -120,6 +171,7 @@ export default class Dashoboard extends React.Component {
                    <TableRowColumn>AD 234</TableRowColumn>
                    <TableRowColumn>
                      <IconButton
+                       onTouchTap={this.dialogOpen.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconEditButton}
                        tooltip="Edit"
@@ -127,6 +179,7 @@ export default class Dashoboard extends React.Component {
                          <ImageEdit/>
                      </IconButton>
                      <IconButton
+                       onTouchTap={this.dialogAlert.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconUnassignButton}
                        tooltip="Unassign"
@@ -140,6 +193,7 @@ export default class Dashoboard extends React.Component {
                    <TableRowColumn>AD 234</TableRowColumn>
                    <TableRowColumn>
                      <IconButton
+                       onTouchTap={this.dialogOpen.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconEditButton}
                        tooltip="Edit"
@@ -147,6 +201,7 @@ export default class Dashoboard extends React.Component {
                          <ImageEdit/>
                      </IconButton>
                      <IconButton
+                       onTouchTap={this.dialogAlert.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconUnassignButton}
                        tooltip="Unassign"
@@ -160,6 +215,7 @@ export default class Dashoboard extends React.Component {
                    <TableRowColumn>AD 234</TableRowColumn>
                    <TableRowColumn>
                      <IconButton
+                       onTouchTap={this.dialogOpen.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconEditButton}
                        tooltip="Edit"
@@ -167,6 +223,7 @@ export default class Dashoboard extends React.Component {
                          <ImageEdit/>
                      </IconButton>
                      <IconButton
+                       onTouchTap={this.dialogAlert.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconUnassignButton}
                        tooltip="Unassign"
@@ -180,6 +237,7 @@ export default class Dashoboard extends React.Component {
                    <TableRowColumn>AD 234</TableRowColumn>
                    <TableRowColumn>
                      <IconButton
+                       onTouchTap={this.dialogOpen.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconEditButton}
                        tooltip="Edit"
@@ -187,6 +245,7 @@ export default class Dashoboard extends React.Component {
                          <ImageEdit/>
                      </IconButton>
                      <IconButton
+                       onTouchTap={this.dialogAlert.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconUnassignButton}
                        tooltip="Unassign"
@@ -200,6 +259,7 @@ export default class Dashoboard extends React.Component {
                    <TableRowColumn>AD 234</TableRowColumn>
                    <TableRowColumn>
                      <IconButton
+                       onTouchTap={this.dialogOpen.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconEditButton}
                        tooltip="Edit"
@@ -207,6 +267,7 @@ export default class Dashoboard extends React.Component {
                          <ImageEdit/>
                      </IconButton>
                      <IconButton
+                       onTouchTap={this.dialogAlert.bind(this)}
                        style={materialuiDashboardStyle.iconButton}
                        iconStyle={materialuiDashboardStyle.iconUnassignButton}
                        tooltip="Unassign"
@@ -219,6 +280,46 @@ export default class Dashoboard extends React.Component {
              </Table>
            </Paper>
          </div>
+
+         <Dialog
+          title="Edit Connection"
+          actions={editActions}
+          modal={false}
+          open={this.state.dialogEdit}
+          onRequestClose={this.dialogClose.bind(this)}>
+            <div className={style.dropDown}>
+              <SelectField
+                floatingLabelText="Select Agent"
+                floatingLabelFixed={true}
+                floatingLabelStyle={materialuiDashboardStyle.floatingLabelStyle}
+                maxHeight={300}
+                value={this.state.agentValue}
+                onChange={this.agentChange.bind(this)}>
+                {agents}
+              </SelectField>
+            </div>
+
+            <div className={style.dropDown}>
+              <SelectField
+                floatingLabelText="Select vehicle"
+                floatingLabelFixed={true}
+                floatingLabelStyle={materialuiDashboardStyle.floatingLabelStyle}
+                maxHeight={300}
+                value={this.state.vehicleValue}
+                onChange={this.vehicleChange.bind(this)}>
+                {vehicles}
+              </SelectField>
+            </div>
+        </Dialog>
+
+        <Dialog
+         title="Unassign Connection"
+         actions={alertActions}
+         modal={false}
+         open={this.state.dialogAlert}
+         onRequestClose={this.dialogClose.bind(this)}>
+         Do you realy want to unassign agent and vehicle connection?
+       </Dialog>
 
      </div>
     );
