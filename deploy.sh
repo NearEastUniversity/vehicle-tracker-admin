c94c8bac14883bb18be0a9016a7581470e039d0d
Mustafa Arici <mustafa@arici.io>
2017-09-29 21:42:46 +0300

    chore(ci): hopefully fix the ci

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -x
 
 openssl aes-256-cbc -K $encrypted_44f843209bfd_key -iv $encrypted_44f843209bfd_iv -in priv.enc -out /tmp/id_rsa -d
 chmod 600 /tmp/id_rsa
 
-rsync -r --delete-after -Pav -e "ssh -i /tmp/id_rsa/id_rsa" $TRAVIS_BUILD_DIR/public root@api.neu.edu.tr:/srv/vehicle-tracker-admin/
+rsync -r --delete-after -Pav -e "ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/public core@api.neu.edu.tr:/srv/vehicle-tracker-admin/
 
 rm -rf /tmp/id_rsa
